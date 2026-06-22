849cf2bf83d2f012c925b24907e80e007d4f7512
cocoleghari <hickmatyarlaghari@gmail.com>
2026-06-22 16:40:53 +0700

    add favicon

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,6 +3,7 @@
 
 <head>
     <meta charset="UTF-8">
+    <link rel="icon" type="image/png" href="{{ asset('images/favicon.png') }}">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login Admin - KaryaOne</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
